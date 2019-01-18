1615076b74292fc01718877ded8e4bf7b57437c1
Damien Stanton <damien.stanton@gmail.com>
2019-01-18 10:42:00 -0500

    first lexer tests passing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use compiler::simple_math::inc;
-
 fn main() {
-    println!("1+1={}", inc(1));
+    unimplemented!();
 }
--- tests/test.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests {
-    use compiler::simple_math::inc;
-    
-    #[test]
-    fn smoke() {
-        assert_eq!(inc(1), 2);
-    }
-}
